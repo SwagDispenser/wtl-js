94332dfb101812c92b622e9d64a9fafc6a52f239
dmytro <dimadovgesh@gmail.com>
2026-03-10 08:28:54 +0200

    dorobyv ex1

--- LAB-02/ex1.js
+++ LAB-02/ex1.js
@@ -7,3 +7,19 @@ alert(max);
 let min = Math.min.apply(null, arr);
 alert(min);
 
+let obj1 = {
+    name: "Dmytro",
+    age: 20
+};
+
+let obj2 = {
+    name: "Dmytro",
+    age: 20
+};
+
+// порівняння властивостей
+if (obj1.name === obj2.name && obj1.age === obj2.age) {
+    alert("Об'єкти однакові");
+} else {
+    alert("Об'єкти різні");
+}
